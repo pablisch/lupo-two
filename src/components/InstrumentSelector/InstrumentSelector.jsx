5fe39c4c999eff406b12c9b006f322517fb0e1a2
Pablo <pablito@me.com>
2024-06-03 20:54:30 +0100

    First styling of instrument buttons with indicator light.

--- src/components/InstrumentSelector/InstrumentSelector.jsx
+++ src/components/InstrumentSelector/InstrumentSelector.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './InstrumentSelector.css';
 
-const InstrumentSelector = ({instrumentLabel, instrumentSet, currentInstrument, changeCurrentInstrument}) => {
+const InstrumentSelector = ({
+  instrumentLabel,
+  instrumentSet,
+  currentInstrument,
+  changeCurrentInstrument,
+}) => {
   return (
-    <div>
+    <div className='instrument-selector-container'>
       <button
-        className='instrumentButton'
+        className='instrument-button'
         id={instrumentSet}
         onClick={() => changeCurrentInstrument(instrumentSet)}
         disabled={currentInstrument === instrumentSet}>
         {instrumentLabel}
       </button>
+      <div className={`instrument-light ${currentInstrument === instrumentSet ? 'instrument-light-active' : ''}`}></div>
     </div>
-  )
-}
+  );
+};
 
-export default InstrumentSelector
+export default InstrumentSelector;
